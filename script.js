9e839e3168249addc67ab976b14f425cd2a3bd56
Josh Seckman <jseckman90@gmail.com>
2021-02-23 14:30:20 -0500

    cleaned up validation code from 4 if statements to a single if statement

--- script.js
+++ script.js
@@ -24,34 +24,25 @@ const isValidEmail = (email) => {
   return re.test(String(email).toLowerCase());
 };
 
+// Check required fields
+const checkRequired = (inputArray) => {
+  inputArray.forEach((input) => {
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is Required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+};
+
+const getFieldName = (input) => {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+};
+
 //Event listeners
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  if (username.value === "") {
-    showError(username, "Username is required");
-  } else {
-    showSuccess(username);
-  }
-
-  if (email.value === "") {
-    showError(email, "Email is required");
-  } else if (!isValidEmail(email.value)) {
-    showError(email, "Email is not valid");
-  } else {
-    showSuccess(email);
-  }
-
-  if (password.value === "") {
-    showError(password, "Password is required");
-  } else {
-    showSuccess(password);
-  }
-
-  if (password2.value === "") {
-    showError(password2, "Password is required");
-  } else {
-    showSuccess(password2);
-  }
+  checkRequired([username, email, password, password2]);
 });
